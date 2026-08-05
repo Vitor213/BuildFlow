f332d8fcbe22563d5c2db0943ba2c238dd7c75d2
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2026-08-05 09:55:51 -0300

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3002",
 });
 
 api.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
-    const token = window.localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
