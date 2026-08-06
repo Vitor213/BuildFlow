e056878fbf87df4acbc2e3fb77d7d08638107087
Vitor213 <145076909+Vitor213@users.noreply.github.com>
2026-08-06 14:12:09 -0300

    Update sale.service.ts

--- backEnd/src/sale/sale.service.ts
+++ backEnd/src/sale/sale.service.ts
@@ -112,7 +112,9 @@ export class SaleService {
     });
   }
 
-  remove() {
+  remove(id: number) {
+    console.debug(id);
+
     throw new BadRequestException(
       'Vendas não podem ser excluídas para preservar o histórico.',
     );
